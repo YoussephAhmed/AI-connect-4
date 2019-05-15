3079fb90c380077f5db4954bb902f00f2965be9a
Youssef Ahmed <yousseffahmed@hotmail.com>
2019-05-15 17:03:12 +0200

    update the utility function in the classes

--- classes.py
+++ classes.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pygame
 import sys
-
-
+import copy
+import math
 # Global constants for the GUI Setup
 SQUARESIZE = 100
 COL_NO = 7
@@ -11,99 +11,101 @@ ROW_NO = 6
 
 class Player:
     player_id = int # to determine player 1 or 2
-    score = int # to define which player have high probability of winning at this point of the game
+
+    # 1 -> user
+    # 2 -> ai
+
 
     def __init__(self, player_id):
         self.player_id = player_id
-        self.score = 0
 
     def play_move(self,board,j):
+
+        #board here is numpy array
 
         for i in range(6):
             if board[5-i, j] == 0:
                 board[5-i, j] = self.player_id
                 return
-######################__________board_evaluation_function_with_some_helping_functions__________###########################################
-  #helping functions
-    def there_is_three_in_a_row(self,board):
-        row_1 = np.array([self.player_id, self.player_id, self.player_id])
-        for i in range(6):
-            for j in range(3):
-                if (np.array_equal(board[i, j : j+3], row_1)):
-                    return 1
-        
-        return 0
 
-    def there_is_two_in_a_row(self,board,opponent):
-        if opponent:
-            if self.player_id==1:
-                row_1=np.array([2,2])
-            else:
-                row_1=np.array([1,1])
+
+
+    # returns ( score , best_j )
+    def best_ai_move(self,board, alpha=- math.inf,beta=math.inf , turn = 2, depth = 10): #recusrive
+
+        #board here is an object
+        board_copy = copy.deepcopy(board)
+        if(board_copy.full() and not board_copy.win()): # tie is happened at the leave node
+            score = 0
+            return score, None
+
+        else
+
+
+
+
+        if depth == 0
+            p = function_younan()
+            return p, 0
+        depth -= 1
+
+        # alpha_new = alpha
+        # beta_new = beta
+        a = alpha
+        b = beta
+
+
+        best_play = 0
+        for i in range (7):
+
+            if(board_copy.possible(i)):
+                board_copy.play(turn, i)
+                if board_copy.win():
+                    if turn == 2:
+                        return 100
+                    else:
+                        return -100
+                    continue
+                if turn == 2:
+                    a, _ = self.best_ai_move(board_copy, a, b, 1, depth)
+                    if a < alpha:
+                        a = alpha
+                    else :
+                        best_play = i
+
+                else :
+                    b, _ = self.best_ai_move(board_copy, a, b, 2, depth)
+                    if b > beta:
+                        b = beta
+                    else:
+                        best_play = i
+
+                board_copy.undo(i)#function to be made which removes the last coin in this column
+                if a >= b :
+                     if turn == 2 :
+                         return b, best_play
+                     else:
+                         return a, best_play
+
+        if turn == 2:
+            return b, best_play
         else:
-            row_1=np.array([self.player_id,self.player_id,])
-    
-        for i in range (6):
-            for j in range (4):
-                if (np.array_equal(board[i, j : j+2], row_1)):
-                    return 1
-        
-        return 0
-    def there_is_opponent_win_chance(self,board):
-        
-        if self.player_id==1:
-            row_1=np.array([2,2])
-        else:
-            row_1=np.array([1,1])
-    
-        for i in range (6):
-            for j in range (3):
-                if (np.array_equal(board[i, j : j+3], row_1)):
-                    return 1
-        return 0
-
-    def evaluate_state(self,board,next_move):
-        # maximum value = 100 -> absolute win with this next move
-        # minimum value = -100 -> absolute lose with this next move
-                                                                                                
-        win_score=100
-        lose_score=100
-        score=0
-        new_board=GameBoard()
-        new_board.board=np.copy(board)
-        
-        if(new_board.possible(next_move)):
-            self.play_move(new_board.board,next_move)
-           
-        if(new_board.win()):
-            return win_score
-
-        if next_move==3:# board center
-            score+=4
-        
-        if self.there_is_two_in_a_row(new_board.board,opponent=0):
-            score+=3
-
-        if self.there_is_three_in_a_row(new_board.board):
-            score+=5
-        
-        if self.there_is_two_in_a_row(new_board.board,opponent=1):
-            score-=5
-        
-        if self.there_is_opponent_win_chance(new_board.board):
-            return lose_score
-
-        return score
-
-
-############################################################################################################################
-    def smart_move(self,board):
-        #compute the best position
+            return a, best_play
+                #compute the best position
         #call play_move with this move
-        pass
+
+
+    def simulate_ai_move(self,board):
+        j = self.best_ai_move()
+        self.play_move(board.board, j)
+
+
 
 
 class GameBoard:
+    turn = (int)
+    # 1 for user
+    # 2 for ai
 
     def __init__(self):
         self.board = np.zeros((6,7)) # the connect 4 board is 6*7
@@ -166,7 +168,98 @@ class GameBoard:
         return 0
 
     def play(self, player, j):
-        pass
+        for i in range(6):
+            if self.board[5-i, j] == 0:
+                self.board[5-i, j] = player
+                return
+
+    def undo(self, j):
+        for i in range(6):
+            if self.board[5-i, j] != 0:
+                self.board[5-i, j] = 0
+                return
+
+
+                ######################__________board_evaluation_function_with_some_helping_functions__________###########################################
+                # helping functions
+
+    def there_is_three_in_a_row(self, board):
+        row_1 = np.array([self.turn, self.turn, self.turn])
+        for i in range(6):
+            for j in range(3):
+                if (np.array_equal(board[i, j: j + 3], row_1)):
+                    return 1
+
+        return 0
+
+    def there_is_two_in_a_row(self, board, opponent):
+        if opponent:
+            if self.turn == 1:
+                row_1 = np.array([2, 2])
+            else:
+                row_1 = np.array([1, 1])
+        else:
+            row_1 = np.array([self.turn, self.turn, ])
+
+        for i in range(6):
+            for j in range(4):
+                if (np.array_equal(board[i, j: j + 2], row_1)):
+                    return 1
+
+        return 0
+
+    def there_is_opponent_win_chance(self, board):
+
+
+        #if self.player_id == 1:
+        if self.turn == 1:
+            row_1 = np.array([2, 2])
+        else:
+            row_1 = np.array([1, 1])
+
+        for i in range(6):
+            for j in range(3):
+                if (np.array_equal(board[i, j: j + 3], row_1)):
+                    return 1
+        return 0
+
+    def evaluate_state(self, board, next_move):
+        # maximum value = 100 -> absolute win with this next move
+        # minimum value = -100 -> absolute lose with this next move
+
+        win_score = 100
+        lose_score = 100
+        score = 0
+        new_board = GameBoard()
+        new_board.board = np.copy(board)
+
+        if (new_board.possible(next_move)):
+            self.play(new_board.board, next_move)
+
+        if (new_board.win()):
+            return win_score
+
+        if next_move == 3:  # board center
+            score += 4
+
+        if self.there_is_two_in_a_row(new_board.board, opponent=0):
+            score += 3
+
+        if self.there_is_three_in_a_row(new_board.board):
+            score += 5
+
+        if self.there_is_two_in_a_row(new_board.board, opponent=1):
+            score -= 5
+
+        if self.there_is_opponent_win_chance(new_board.board):
+            return lose_score
+
+        return score
+
+
+############################################################################################################################
+
+
 
 
 class Game:
